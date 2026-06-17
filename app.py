a0b95da242fb78655512e3f048b5178e119d10da
yg <r210007@outlook.com>
2026-06-17 11:43:54 +0800

    creating new pages

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.title("Hello, Streamlit 👋")
-st.write("This is your first Streamlit app.")
+st.title("About")
+st.write("This app helps manage bread production and inventory.")
+st.write("Use the sidebar to navigate to Bread Overview or Manage Bread.")

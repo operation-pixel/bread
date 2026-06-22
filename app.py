9056b9a7d67e266dd08d446e164752e0ca70d4ca
yg <r210007@outlook.com>
2026-06-22 10:54:10 +0800

    ok

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 import streamlit as st
-st.session_state.bread_data.to_excel(EXCEL_FILE, index=False, engine="openpyxl")
-
 
 st.title("About")
 st.write("This app helps manage bread production and inventory.")
 st.write("Use the sidebar to navigate to Bread Overview or Manage Bread.")
 
-
-import os
-import streamlit as st
-final = "burger.jpeg"
-APP_DIR = os.path.dirname(os.path.abspath(__file__))
-image_path = os.path.join(APP_DIR, "pages", "Images", final)
-
-st.image(image_path, caption="Burger", width=100)
 
 
